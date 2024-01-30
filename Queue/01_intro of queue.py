71db37ae035c3df76dbbb25a310ebdbe5dded2b1
Abhijit Das <abhijitd721201@gmail.com>
2024-01-30 14:45:30 +0530

    first commit

---
+++ Queue/01_intro of queue.py
@@ -0,0 +1,24 @@
+'''
+What is Queue ?
+
+= A Queue is a linear structure which follows a particular order
+in which the operations are performed. The order is First In First Out (FIFO).
+ A good example of a queue is any queue of consumers for a resource where the consumer that came first is served first. 
+ The difference between stacks and queues is in removing. In a stack we remove the item the most recently added; in a queue,
+  we remove the item the least recently added.
+
+
+Enqueue= The prosess of adding the element in queue is called enqueue..
+
+Dequeue= The prosess of removing the element in queue is called dequeue..
+
+Most important operation of queue=1) enqueue
+                                  2) dequeue
+                                  3) isfull
+                                  4) isempty
+
+Used of Queue= Serving requests on a single shared resource, like a printer,
+              CPU task scheduling etc. In real life scenario, Call Center phone systems
+              uses Queues to hold people calling them in an order, until a service representative
+              is free. Handling of interrupts in real-time systems.
+'''
