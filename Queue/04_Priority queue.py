e65f60ce21e1465fc94df871335d84933c9b5814
Abhijit Das <abhijitd721201@gmail.com>
2024-01-30 15:20:12 +0530

    first commit

---
+++ Queue/04_Priority queue.py
@@ -0,0 +1,30 @@
+'''
+Priority Queue is an extension of queue with following properties.
+
+1) Every item has a priority associated with it.
+2) An element with high priority is dequeued before an element with low priority.
+3) If two elements have the same priority, they are served according to their order in the queue.
+'''
+# lowest element -> highest priority
+# highiest element -> lowest priority
+
+# prority impementation using 1) list  
+                            # 2) PriorityQueue()   CLASS
+
+'''Implementation using List'''
+q=[]
+q.append(2)
+q.append(3)
+q.append(1)
+q.append(4)
+
+q.sort()
+print(q)                #output=> [1, 2, 3, 4]
+
+
+# To remove the element..
+q.pop(0)
+q.pop(0)
+print(q)            #output=> [3, 4]
+
+
